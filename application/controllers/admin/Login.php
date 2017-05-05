d38d397692d084b6f821e9a9e2fab0c5fa20b1d0
pvtam2a <phamtam34@gmail.com>
2017-05-06 00:20:28 +0700

    #update2

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -5,3 +5,10 @@
  * Date: 5/5/2017
  * Time: 11:22 PM
  */
+class Login extends MY_controller
+{
+    function index()
+    {
+        echo "login";
+    }
+}
--- application/core/MY_controller.php
+++ application/core/MY_controller.php
@@ -5,3 +5,33 @@
  * Date: 5/5/2017
  * Time: 11:17 PM
  */
+class MY_controller extends CI_Controller
+{
+    function __construct()
+    {
+        //Ke_thua_tu_CI_Controller
+        parent::__construct();
+        $controller = $this->uri->segment(1);
+        switch ($controller)
+        {
+            case 'admin':
+            {
+                $this->_check_login();
+                //xử lý các dữ liệu khi truy cập vào trang admin
+                echo 'bạn đang truy cập vào hệ thống quản lý';
+                break;
+            }
+            default:
+            {
+                //xử lý các dữ liệu ở trang ngoài
+            }
+        }
+    }
+    /*
+     * Kiem tra trang thai dang nhap cua admin
+     */
+    private function _check_login()
+    {
+        echo 'abc';
+    }
+}
