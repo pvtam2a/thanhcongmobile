76aa6ed446892800ec34942099b16aa4f8f911ad
pvtam2a <phamtam34@gmail.com>
2017-05-09 23:15:33 +0700

    #cap nhat code them tai khoan quan tri video8

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -28,7 +28,47 @@ class Admin extends MY_controller
 
     function add()
     {
+        $this->load->library('form_validation');
+        $this->load->helper('form');
+        //neu co du lieu post len thi check
+        if ($this->input->post()) {
+            $this->form_validation->set_rules('name', 'Họ Và Tên', 'required|min_length[8]');
+            $this->form_validation->set_rules('username', 'Tên Đăng Nhập', 'required|callback_check_username');
+            $this->form_validation->set_rules('password', 'Mật Khẩu', 'required|min_length[6]');
+            $this->form_validation->set_rules('re_password', 'Nhập lại mật khẩu', 'required|matches[password]');
+            if($this->form_validation->run())
+            {
+                //thêm vào csdl
+                $name = $this->input->post('name');
+                $username = $this->input->post('username');
+                $password = $this->input->post('password');
+                $data = array(
+                    'name' => $name,
+                    'username' => $username,
+                    'password' => md5($password)
+                );
+                if ($this->admin_model->create($data)) {
+                    echo 'Thêm thành công';
+                } else {
+                    echo 'Không thêm thành công';
+                }
+            }
+        }
         $this->data['temp'] = 'admin/admin/add';
         $this->load->view('admin/main', $this->data);
+    }
+
+    function check_username()
+    {
+        //kiểm tra username đã tồn tại chưa
+        $username = $this->input->post('username');
+        $where = array('username' => $username);
+        //kiểm tra username đã tồn tại hay chưa
+        if ($this->admin_model->check_exists($where)) {
+            //trả về thông báo lỗi
+            $this->form_validation->set_message(__FUNCTION__, 'Tài khoản đã tồn tại');
+            return false;
+        }
+        return true;
     }
 }
--- application/views/admin/admin/add.php
+++ application/views/admin/admin/add.php
@@ -9,16 +9,47 @@
         <div class="title">
             <h6>Thêm mới quản trị viên</h6>
         </div>
-        <form class="form" id="form" action="product/add.html" method="post" enctype="multipart/form-data">
+        <form class="form" id="form" action="" method="post" enctype="multipart/form-data">
             <fieldset>
                 <div class="formRow">
                     <label class="formLeft" for="param_name">Tên:<span class="req">*</span></label>
                     <div class="formRight">
-                        <span class="oneTwo"><input name="name" id="param_name" _autocheck="true" type="text" /></span>
+                        <span class="oneTwo"><input name="name" id="param_name" _autocheck="true" value="<?php echo set_value('name')?>" type="text" /></span>
                         <span name="name_autocheck" class="autocheck"></span>
-                        <div name="name_error" class="clear error"></div>
+                        <div name="name_error" class="clear error"><?php echo form_error('name')?></div>
                     </div>
                     <div class="clear"></div>
+                </div>
+                <div class="formRow">
+                    <label class="formLeft" for="param_username">Username:<span class="req">*</span></label>
+                    <div class="formRight">
+                        <span class="oneTwo"><input name="username" id="param_username" _autocheck="true" value="<?php echo set_value('username')?>" type="text" /></span>
+                        <span name="name_autocheck" class="autocheck"></span>
+                        <div name="name_error" class="clear error"><?php echo form_error('username')?></div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="formRow">
+                    <label class="formLeft" for="param_username">Password:<span class="req">*</span></label>
+                    <div class="formRight">
+                        <span class="oneTwo"><input name="password" id="param_password" _autocheck="true" value="<?php echo set_value('password')?>" type="password" /></span>
+                        <span name="name_autocheck" class="autocheck"></span>
+                        <div name="name_error" class="clear error"><?php echo form_error('password')?></div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="formRow">
+                    <label class="formLeft" for="param_username">Nhập lại mật khẩu:<span class="req">*</span></label>
+                    <div class="formRight">
+                        <span class="oneTwo"><input name="re_password" id="param_re_password" _autocheck="true" value="<?php echo set_value('re_password')?>" type="password" /></span>
+                        <span name="name_autocheck" class="autocheck"></span>
+                        <div name="name_error" class="clear error"><?php echo form_error('re_password')?></div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="formSubmit">
+                    <input type="submit" value="Thêm mới" class="redB" />
+                    <!--<input type="reset" value="Hủy bỏ" class="basic" />-->
                 </div>
             </fieldset>
         </form>
